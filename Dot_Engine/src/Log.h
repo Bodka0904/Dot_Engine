acc32543f2dcf6aa51d6244fc38ad1e90f0ae63f
Bodka0904 <44289637+Bodka0904@users.noreply.github.com>
2019-05-17 14:05:08 +0200

    Update Log.h

--- Dot_Engine/src/Log.h
+++ Dot_Engine/src/Log.h
@@ -10,10 +10,6 @@ public:
 
 	inline static void SetLevel(unsigned int Level);
 	inline static void SetColor(unsigned int color);
-	inline static void Message(std::string caller,std::string msg);
-
-
-	static unsigned int LEVEL;
 
 	enum 
 	{
@@ -25,7 +21,9 @@ public:
 
 		TRACE
 	};
-
+private:
+	static unsigned int LEVEL;
+	
 	enum
 	{
 		RED = 20,
@@ -35,6 +33,6 @@ public:
 	
 };
 
-#define LOG_INFO(caller,msg) {if(Log::LEVEL == Log::INFO || Log::LEVEL == Log::TRACE) {Log::SetColor(Log::WHITE);Log::Message(caller,msg);}}
-#define LOG_WARN(caller,msg) {if(Log::LEVEL == Log::WARN || Log::LEVEL == Log::TRACE) {Log::SetColor(Log::YELLOW);Log::Message(caller, msg);}}
-#define LOG_ERR(caller,msg) {if(Log::LEVEL == Log::ERR || Log::LEVEL == Log::TRACE) {Log::SetColor(Log::RED);Log::Message(caller, msg);}}
+#define LOG_INFO(pargs) {if(Log::LEVEL == Log::INFO || Log::LEVEL == Log::TRACE) {Log::SetColor(Log::WHITE); printf pargs;}}
+#define LOG_WARN(pargs) {if(Log::LEVEL == Log::WARN || Log::LEVEL == Log::TRACE) {Log::SetColor(Log::YELLOW);printf pargs;}}
+#define LOG_ERR(pargs) {if(Log::LEVEL == Log::ERR || Log::LEVEL == Log::TRACE) {Log::SetColor(Log::RED);printf pargs;}}
